753cb74dce3ba2aac71e88b0420008c1c66583bd
Cintia Losada <cinlos3382@gmail.com>
2024-10-10 13:40:41 -0300

    imagenes en colecciones

--- 02-blog/src/content/config.ts
+++ 02-blog/src/content/config.ts
@@ -2,11 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 const BlogCollection = defineCollection({
     type: 'content',
-    schema: z.object({
+    schema: ({ image  }) => 
+        z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: z.string(),
+        image: image().refine((img) => img.width < 1200, {
+            message: "Image must be at least 1200px wide"
+        }),
 
         //Relación
         tags: z.array(z.string()),
